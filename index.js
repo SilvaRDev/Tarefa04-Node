4e6760e7ae09d4f8e7facc9315e17f1ccfc8b0ec
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-06-25 10:03:09 -0300

    Atualização no index.js e main.handlebars
    Modificações no header e adição de rotas

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.set('views', './views')
 
 app.use(express.static('public'))
 
+app.get('/produtos', (req, res) => {
+  res.render('products')
+})
+
+app.get('/entrar', (req, res) => {
+  res.render('login')
+})
+
+app.get('/registrar', (req, res) => {
+  res.render('register')
+})
+
 app.get('/', (req, res) => {
   res.render('home')
 })
