7491b39fd766f16fa0f259cabf76a0511fca9f49
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-06-25 18:06:20 -0300

    Adicionado menu de cards em /produtos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 const exphbs = require('express-handlebars')
 
@@ -7,16 +8,22 @@ const hbs = exphbs.create({
   partialsDir: ['views/partials']
 })
 
-const port = 3000
-
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
 app.use(express.static('public'))
 
+const products = [
+  {id: 1, name: 'Porta', description: 'Porta feita com madeira de carvalho', value: 1999},
+  {id: 2, name: 'Abajur', description: 'Abajur novo, fabricado na China', value: 89},
+  {id: 3, name: 'Teclado', description: 'Teclado logitech novo, ainda na caixa', value: 199},
+  {id: 3, name: 'Teclado', description: 'Teclado logitech novo, ainda na caixa', value: 199},
+  {id: 3, name: 'Teclado', description: 'Teclado logitech novo, ainda na caixa', value: 199}
+]
+
 app.get('/produtos', (req, res) => {
-  res.render('products')
+  res.render('products', { products })
 })
 
 app.get('/entrar', (req, res) => {
@@ -28,7 +35,7 @@ app.get('/registrar', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', { products })
 })
 
 app.listen(port, () => {
